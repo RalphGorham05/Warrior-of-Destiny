f7b15a41d5e3911341e29e30dd44d6ecc1c91c24
RalphGorham05 <Rgorham1@gaels.iona.edu>
2015-04-21 16:00:07 -0400

    started Monk class

---
+++ Monk.py
@@ -0,0 +1,13 @@
+'''
+4/21/15
+Monk class
+'''
+
+from Character import Character
+
+class Monk(Character):
+    def__init__(self, characterLevel, ):
+        Character.__init__(self)
+        self.aliveStatus = True
+        
+
