acb84764469ae5b0c45943179c595037d42bc5b0
RalphGorham05 <Rgorham1@gaels.iona.edu>
2015-04-22 16:00:11 -0400

    minor Monk stuff

--- Monk.py
+++ Monk.py
@@ -9,5 +9,13 @@ class Monk(Character):
     def__init__(self, characterLevel, ):
         Character.__init__(self)
         self.aliveStatus = True
+        self.attackList = ['attack1', 'attack2', 'attack3']
+        self.attributes = {'Strength':'', 'Luck':'', 'IQ':'', 'Stealth':'', 'Speed':'', 'Chi':''}
+
+    def getChi(self):
+        chi = self.attributes['Chi']
+        return chi
+        
+
         
 
